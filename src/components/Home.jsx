fdaf2cf2761d6af53f8dde0e28a5d861ab257f85
Matt <matthew.sheehan94@gmail.com>
2023-07-18 10:56:06 +0100

    add single article page functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, CSSProperties } from 'react'
+import { useState, useEffect } from 'react'
 import SearchBar from './SearchBar'
 import { getArticles } from '../utils/apis'
 import ArticlePreview from './ArticlePreview'
---
+++ src/utils/convertDate.js
@@ -0,0 +1,41 @@
+const convertDate = (ISODate) => {
+
+    console.log(ISODate, "<<<< param")
+    const months = {
+        "01": "January",
+        "02": "February",
+        "03": "March",
+        "04": "April",
+        "05": "May",
+        "06": "June",
+        "07": "July",
+        "08": "August",
+        "09": "September",
+        "10": "October",
+        "11": "November",
+        "12": "December"
+    }
+
+    const splitDate = ISODate.split("-")
+    const year = splitDate[0]
+    const month = months[splitDate[1]]
+    const day = splitDate[2].slice(0, 2)
+    
+    let suffix
+    switch (day) {
+        case day[1] === "1":
+            suffix= "st"
+        case day[1] === "2":
+            suffix = "nd"
+        default:
+            suffix = "th"
+    }
+
+    let formattedDay
+    if (day[0] === "0") formattedDay = day[1] + suffix
+    else formattedDay = day + suffix
+
+    return formattedDay + " " + month + " " + year
+}
+
+export default convertDate
