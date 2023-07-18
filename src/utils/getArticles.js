867e02598eb982e682f3e67eb92fff2a85c88633
Matt <matthew.sheehan94@gmail.com>
2023-07-18 09:42:11 +0100

    remove leftover utils file

--- src/utils/getArticles.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({ baseURL: 'https://nc-news-api-45fk.onrender.com/api'})
-
-export const getArticles = (resultsPage) => {
-    return api.get(`/articles?p=${resultsPage}&total_count=true`).then(res => res.data)
-}
