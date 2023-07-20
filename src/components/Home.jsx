ba7d360106de47c4be7a303d397993f12f152628
Matt <matthew.sheehan94@gmail.com>
2023-07-20 23:06:17 +0100

    remove unnecessary code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,15 @@
 import { useParams } from 'react-router-dom'
 import { useState } from 'react'
 import ResultsList from './ResultsList'
-import SortOptions from './SortOptions'
 
 const Home = ({ allTopics }) => {
     const params = useParams()
-    const [order, setOrder] = useState("DESC")
-    const [sortBy, setSortBy] = useState("date")
-
-    console.log(order, "<<<< order")
 
     return <section id="">
         <div className="articles-header">
         <h3>{params.topic ? params.topic : "Recent"} Articles:</h3>
-        < SortOptions order={order} setOrder={setOrder} sortBy={sortBy} setSortBy={setSortBy} />
         </div>
-        < ResultsList allTopics={allTopics} topic={params.topic} order={order} sortBy={sortBy} />
+        < ResultsList allTopics={allTopics} topic={params.topic} />
     </section>
 }
 
